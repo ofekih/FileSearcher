b3acda7d6b98b437df529e28b48e23d66c988577
Ofek Gila <flamingo.ofek@gmail.com>
2015-05-25 22:48:49 -0700

    Added javadoc

--- FileSearcher.java
+++ FileSearcher.java
@@ -24,17 +24,30 @@ public final class FileSearcher	{
 	 * Searches EVERYTHING in the computer
 	 */
 	public final static int SEARCH_EVERYTHING = 3;
-
+	/**
+	 * The current directory as a file
+	 */
 	public final static File currentDirectory = new File(System.getProperty("user.dir"));
 
 	private FileSearcher()	{}	// to prevent initialization
 
 	private static File file = null;
-
+	/**
+	 * Finds the first instance of a file that matches the name
+	 * @param  name       The name of the file with or without the extension
+	 * @param  searchType The method of searching
+	 * @return            The found File
+	 */
 	public static File findFile(String name, int searchType)	{
 		return findFile(name, currentDirectory, searchType);
 	}
-
+	/**
+	 * Finds the first instance of a file that matches the name
+	 * @param  name          The name of the file with or without the extension
+	 * @param  currentFolder The folder to start searching from if only searching currentfolder and or subfolders
+	 * @param  searchType    The method of searching
+	 * @return               The found File
+	 */
 	public static File findFile(String name, File currentFolder, int searchType)	{
 		findFileR(getName(name), getExtension(name), getFolder(currentFolder, searchType), searchType);
 		return file;
@@ -50,11 +63,22 @@ public final class FileSearcher	{
 					file = fileEntry;
 		}	catch(NullPointerException e)	{}
 	}
-
+	/**
+	 * Finds all the files that match the name
+	 * @param  name       The name to find with or without the extension
+	 * @param  searchType The method of searching
+	 * @return            All the files found
+	 */
 	public static ArrayList<File> findFiles(String name, int searchType)	{
 		return findFiles(name, currentDirectory, searchType);
 	}
-
+	/**
+	 * Finds all the files that match the name
+	 * @param  name          The name to find with or without the extension
+	 * @param  currentFolder The folder to start searching from if only searching currentfolder and or subfolders
+	 * @param  searchType    The method of searching
+	 * @return               All the files found
+	 */
 	public static ArrayList<File> findFiles(String name, File currentFolder, int searchType)	{
 		if (getName(name).equals("*"))
 			return findFiles(currentFolder, searchType, getExtension(name));
@@ -71,11 +95,22 @@ public final class FileSearcher	{
 		}	catch(NullPointerException e)	{}
 		return files;
 	}
-
+	/**
+	 * Finds all the files that have one of the given extensions
+	 * @param  searchType The method of searching
+	 * @param  extensions Any amount of Strings of extensions to search for (blank will find all)
+	 * @return            All the files found
+	 */
 	public static ArrayList<File> findFiles(int searchType, String... extensions)	{
 		return findFiles(currentDirectory, searchType, extensions);
 	}
-
+	/**
+	 * Finds all the files that have one of the given extensions
+	 * @param  currentFolder The folder to start searching from if only searching currentfolder and or subfolders
+	 * @param  searchType    The method of searching
+	 * @param  extensions    Any amount of Strings of extensions to search for (blank will find all)
+	 * @return               All the files found
+	 */
 	public static ArrayList<File> findFiles(File currentFolder, int searchType, String... extensions)	{
 		return findFilesR(getFolder(currentFolder, searchType), searchType, getExtensions(extensions), new ArrayList<File>());
 	}
@@ -95,11 +130,20 @@ public final class FileSearcher	{
 		}	catch(NullPointerException e)	{}
 		return files;
 	}
-
+	/**
+	 * Prints the files with the given extensions
+	 * @param searchType The method of searching
+	 * @param extensions The extensions to search for
+	 */
 	public static void printFiles(int searchType, String... extensions)	{
 		printFiles(currentDirectory, searchType, extensions);
 	}
-
+	/**
+	 * Prints the files with the given extensions
+	 * @param currentFolder The folder to start searching from if only searching currentfolder and or subfolders
+	 * @param searchType    The method of searching
+	 * @param extensions    The extensions to search for
+	 */
 	public static void printFiles(File currentFolder, int searchType, String... extensions)	{
 		printFilesR(getFolder(currentFolder, searchType), searchType, getExtensions(extensions));
 	}
@@ -118,11 +162,22 @@ public final class FileSearcher	{
 					}
 		}	catch(NullPointerException e)	{}
 	}
-
+	/**
+	 * Finds a folder with the given name
+	 * @param  name       The name to search for
+	 * @param  searchType The method of searching
+	 * @return            A File represenation of the folder
+	 */
 	public static File findFolder(String name, int searchType)	{
 		return findFolder(name, currentDirectory, searchType);
 	}
-
+	/**
+	 * Finds a folder with the given name
+	 * @param  name          The name to search for
+	 * @param  currentFolder The folder to start searching from if only searching currentfolder and or subfolders
+	 * @param  searchType    The method of searching
+	 * @return               A File representation of the folder
+	 */
 	public static File findFolder(String name, File currentFolder, int searchType)	{
 		findFolderR(name, getFolder(currentFolder, searchType), searchType);
 		return file;
@@ -139,11 +194,22 @@ public final class FileSearcher	{
 						findFolderR(name, fileEntry, searchType);
 		}	catch(NullPointerException e)	{}
 	}
-
+	/**
+	 * Finds all folders with the given name
+	 * @param  name       The name to search for
+	 * @param  searchType The method of searching
+	 * @return            The folders found
+	 */
 	public static ArrayList<File> findFolders(String name, int searchType)	{
 		return findFolders(name, currentDirectory, searchType);
 	}
-
+	/**
+	 * Finds all the folders with the given names
+	 * @param  name          The name to search for
+	 * @param  currentFolder The folder to start searching from if only searching currentfolder and or subfolders
+	 * @param  searchType    The method of searching
+	 * @return               The folders found
+	 */
 	public static ArrayList<File> findFolders(String name, File currentFolder, int searchType)	{
 		return findFoldersR(name, getFolder(currentFolder, searchType), searchType, new ArrayList<File>());
 	}
@@ -161,7 +227,7 @@ public final class FileSearcher	{
 		return folders;
 	}
 
-	public static File getFolder(File folder, int searchType)	{
+	private static File getFolder(File folder, int searchType)	{
 		switch (searchType)	{
 			case SEARCH_USER:
 				while(folder.getParentFile().getParentFile().getParentFile() != null)
@@ -174,15 +240,15 @@ public final class FileSearcher	{
 			default: return folder;
 		}
 	}
-	public static String getExtension(String name)	{
+	private static String getExtension(String name)	{
 		return name.lastIndexOf(".") == -1 ? "":name.substring(name.lastIndexOf(".")).substring(1);
 	}
 
-	public static String getName(String name)	{
+	private static String getName(String name)	{
 		return name.substring(name.lastIndexOf("\\")+1, name.lastIndexOf(".") == -1 ? name.length():name.lastIndexOf("."));
 	}
 
-	public static String[] getExtensions(String[] extensions)	{
+	private static String[] getExtensions(String[] extensions)	{
 		for (int i = 0; i < extensions.length; i++)
 			if (!getExtension(extensions[i]).equals(""))
 				extensions[i] = getExtension(extensions[i]);
